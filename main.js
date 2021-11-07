30afe483d47291c0baf4051e978fe4a7630a24be
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2021-11-07 14:36:38 -0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,349 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+function Character() {
+    document.getElementById("Characters").style.display = 'block';
+    document.getElementById("CharacterB").style.display = 'none';
+}
+
+function Junior() {
+    fabric.Image.fromURL('Junior.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Joseph() {
+    fabric.Image.fromURL('Joseph.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Cody() {
+    fabric.Image.fromURL('Cody.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Jeffy() {
+    fabric.Image.fromURL('Jeffy.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Chef_Pee_Pee() {
+    fabric.Image.fromURL('Chef Pee Pee.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Brooklyn_Guy() {
+    fabric.Image.fromURL('Brooklyn Guy.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Mario() {
+    fabric.Image.fromURL('Mario.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Rosalina() {
+    fabric.Image.fromURL('Rosalina.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Bowser() {
+    fabric.Image.fromURL('Bowser.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Jackie_Chu() {
+    fabric.Image.fromURL('Jackie Chu.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Mr_Goodman() {
+    fabric.Image.fromURL('Mr. Goodman.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+
+
+function Items() {
+    document.getElementById("ItemB").style.display = "none";
+    document.getElementById("Items").style.display = 'block';
+}
+
+function Food() {
+    fabric.Image.fromURL('Food.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(50);
+        player_object.scaleToHeight(75);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Money() {
+    fabric.Image.fromURL('Money.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(50);
+        player_object.scaleToHeight(75);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Zombie() {
+    fabric.Image.fromURL('Zombie.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(50);
+        player_object.scaleToHeight(75);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Steak() {
+    fabric.Image.fromURL('Steak.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(50);
+        player_object.scaleToHeight(75);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Wall() {
+    fabric.Image.fromURL('Wall.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(50);
+        player_object.scaleToHeight(75);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Gun() {
+    fabric.Image.fromURL('Gun.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(50);
+        player_object.scaleToHeight(75);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Black_Yoshi() {
+    fabric.Image.fromURL('Blck Yoshi.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Ken() {
+    fabric.Image.fromURL('Ken.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Dolphin() {
+    fabric.Image.fromURL('Dolphin.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Karen() {
+    fabric.Image.fromURL('Karen.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Bully_Bill() {
+    fabric.Image.fromURL('Bully_Bill.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Nancy() {
+    fabric.Image.fromURL('Nancy.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
+
+function Shrek() {
+    fabric.Image.fromURL('Shrek.png', function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(150);
+        player_object.set({
+            top: 0,
+            left: 0
+        });
+        canvas.add(player_object);
+    });
+
+}
